34534dab0018cb4b7cc8bca7597a6aa8df04bb13
Kevin Nichols <kevnichols2@gmail.com>
2023-09-12 15:02:02 -0400

    added comment

--- src/MagicEightBall.js
+++ src/MagicEightBall.js
@@ -3,6 +3,7 @@ import stockAnswers from "./answers.json";
 import message from "./randomMsg";
 import "./MagicEightBall.css";
 
+// Shows a default msg, then updates the msg and color with a random message from answers.json on click.
 const MagicEightBall = ({answers = stockAnswers}) => {
   const [answer, setAnswer] = useState({
     msg: "Thing of a Question",
